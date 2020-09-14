5538d0a1199508b32268de0f2560d71f38e08c38
RiyaGuha1234 <donaguhachoudhury@gmail.com>
2020-09-15 01:13:37 +0530

    completed bill part is going on

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -200,11 +200,11 @@ class CustomerController extends Controller
     public function showCompletedBills(Request $request)
     {
         $input = ($request->json()->all());
-        $data = JobMaster::select('bill_masters.bill_number',DB::raw('order_masters.id as order_master_id'), DB::raw('karigarh.person_name as karigarh_name'), DB::raw('users.id as customer_id'), DB::raw('karigarh.id as karigarh_id'), 'order_masters.order_number', 'order_masters.date_of_order', 'job_masters.gross_weight', 'products.model_number', 'order_details.size', 'order_details.quantity', 'order_details.price','order_masters.date_of_order', 'job_masters.job_number', 'users.person_name', 'users.address1', 'users.mobile1', 'users.state', 'users.po', 'users.area', 'users.city', 'users.pin', 'job_masters.id', DB::raw("if(order_details.status_id = 100,'COMPLETED',if(order_details.status_id = 40,'NOT STARTED','WORK IN PROGRESS')) as status"))
+        $data = JobMaster::select('bill_masters.bill_number','bill_masters.bill_date',DB::raw('order_masters.id as order_master_id'), DB::raw('karigarh.person_name as karigarh_name'), DB::raw('users.id as customer_id'), DB::raw('karigarh.id as karigarh_id'), 'order_masters.order_number', 'order_masters.date_of_order', 'job_masters.gross_weight', 'products.model_number', 'order_details.size', 'order_details.quantity', 'order_details.price','order_masters.date_of_order', 'job_masters.job_number', 'users.person_name', 'users.address1', 'users.mobile1', 'users.state', 'users.po', 'users.area', 'users.city', 'users.pin', 'job_masters.id', DB::raw("if(order_details.status_id = 100,'COMPLETED',if(order_details.status_id = 40,'NOT STARTED','WORK IN PROGRESS')) as status"))
             ->join('users as karigarh', 'job_masters.karigarh_id', '=', 'karigarh.id')
             ->join('order_details', 'job_masters.order_details_id', '=', 'order_details.id')
             ->join('order_masters', 'order_details.order_master_id', '=', 'order_masters.id')
-//            ->join('bill_masters', 'bill_masters.order_master_id', '=', 'order_masters.id')
+            ->join('bill_masters', 'bill_masters.order_master_id', '=', 'order_masters.id')
             ->join('products', 'order_details.product_id', '=', 'products.id')
             ->join('users', 'order_masters.person_id', '=', 'users.id')
             ->where('job_masters.bill_created','=',1)
